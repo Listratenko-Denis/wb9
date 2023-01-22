ce67a6148422b7fc41c9c08ef66401f1fc5e1c46
Listratenko-Denis <114353064+Listratenko-Denis@users.noreply.github.com>
2023-01-22 14:10:09 +0200

    Add files via upload

---
+++ домашка/ill.js
@@ -0,0 +1,69 @@
+// задание 1
+// const str = 'Hello'
+// const str1 = ' world'
+
+// console.log(str.concat(str1));
+
+
+// задание 2
+// const a = 18
+// const b = 2
+// const c = 5
+
+// console.log(a+b+c);
+
+
+//задание 3
+// const a = 17;
+// const b = 18;
+// const c = a-b;
+// const d = 7
+// const result = c + d
+
+// console.log(result);
+
+
+
+// задание 4
+// const a = 'I learn javascript!'
+
+// console.log(a.replaceAll(' ', '_'));
+
+
+// задание 5
+// const a = 'I learn javascript!'
+
+// console.log(a.length);
+
+
+// задание 6 не сделано
+// const a = 1
+// const b = 2
+// const c =3 
+// const d =4 
+// const arr = [a, b, c, d]
+
+// console.log(arr)
+
+// const d
+
+
+// задание 7 не сделано
+// const a = 'indi Horror S Monstrom Na Unity'
+
+// console.log(a.toUpperCase());
+
+
+// задание 8
+// const arr = ['a',]
+// arr.push( 'y', 't', 'r', 'e', 'w', 'q') 
+// arr.unshift('q', 'w', 'e', 'r', 't', 'y', )      
+
+// console.log(arr);
+
+
+// задание 9 сделано но не так
+// const a = prompt()
+// const b = prompt()
+// console.log("Hello "a" You are "b" years");
+
