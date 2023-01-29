ffe73b78fb058d1c2b1ba8f7b66330b59125b211
Listratenko-Denis <114353064+Listratenko-Denis@users.noreply.github.com>
2023-01-29 14:47:50 +0300

    Add files via upload

---
+++ домашка/Ill.js
@@ -0,0 +1,90 @@
+// ЗАДАНИЕ 2
+// const a = 'rrrrrrrrrrrrrrr'
+// const arr = a.split('')
+
+
+// arr[0] = arr[0].toUpperCase()
+// arr[2] = arr[2].toUpperCase()
+// arr[4] = arr[4].toUpperCase()
+// arr[6] = arr[6].toUpperCase()
+// arr[8] = arr[8].toUpperCase()
+// arr[10] = arr[10].toUpperCase()
+// arr[12] = arr[12].toUpperCase()
+// arr[14] = arr[14].toUpperCase()
+// console.log(arr.join(''))
+
+
+
+// ЗАДАНИЕ 3
+// const min = 
+
+// if (min >= 1 && min < 15) {console.log('в 1-ой четверти');} ;
+// if (min >= 15 && min < 30) {console.log('в 2-ой четверти');} ;
+// if (min >= 30 && min < 45) {console.log('в 3-ей четверти');} ;
+// if (min >= 45 && min <= 60) {console.log('в 4-ой четверти');} ;
+
+
+// ЗАДАНИЕ 4   (понедельник, tuesday, 水曜日, Jeudi, Freitag, rāhoroi, 星期日)
+// const lang = ''
+
+// if (lang === 'понедельник') {console.log('понедельник')}
+// if (lang === 'tuesday') {console.log('вторник')}
+// if (lang === '水曜日') {console.log('среда')}
+// if (lang === 'Jeudi') {console.log('четверг')}
+// if (lang === 'Freitag') {console.log('пятница')}
+// if (lang === 'rāhoroi') {console.log('суббота')}
+// if (lang === '星期日') {console.log('воскресеньсе')}
+
+
+// ЗАДАНИЕ 5
+// const lange = ''
+
+// switch (lange) {
+//  case 'понедельник':
+//   console.log('понедельник');
+//   break;
+  
+//   case 'tuesday':
+//    console.log('вторник');
+//   break;
+  
+//   case '水曜日':
+//    console.log('среда');
+//   break;
+
+//   case 'Jeudi':
+//     console.log('четверг');
+//    break;
+
+//    case 'biyernes':
+//     console.log('пятница');
+//    break;
+
+//    case 'rāhoroi':
+//     console.log('суббота');
+//    break;
+
+//    case '水曜日':
+//     console.log('воскресенье');
+//    break;
+
+//  default:
+//  console.log('неправильно введено');
+//   break;
+// }
+
+
+// ЗАДАНИЕ 9
+// const a = random = Math.floor(Math.random() * 10)
+// console.log(a);
+// const b = prompt()
+// if (a > b) {console.log('много');} 
+// if (a < b) {console.log('мало');} 
+// if (a = b) {console.log('нармална');}
+
+
+// ЗАДАНИЕ 10
+// const a = 11
+// const b = 20
+
+// if (a <= 10 || a >= 45 || b <= 4 || b >= 26 || a == 23) {console.log('ой, что-то пошло не по плану')} else {console.log(a * b);}
