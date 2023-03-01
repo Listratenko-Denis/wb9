1e63a662aec6d629bf650d37f572a1ea57ff1a40
Listratenko-Denis <114353064+Listratenko-Denis@users.noreply.github.com>
2023-03-01 20:01:53 +0300

    Add files via upload

---
+++ denis/index.js
@@ -0,0 +1,73 @@
+// const btnId = document.getElementById('btn')
+// console.log(btnId);
+
+// const btnId2 = document.querySelector('#btn')
+// console.log(btnId2);
+
+// const btnclass = document.querySelector('.btn')
+// console.log(btnclass);
+
+// const li = document.querySelectorAll('li')
+// console.log(li);
+
+// for (let elem of li) {console.log(elem.matches('li[value="6"]'));} // elem.matches проверяет 
+
+
+// const chapter = document.querySelector('.chapter')
+// console.log(chapter);
+
+// console.log(chapter.closest('.book'));
+// console.log(chapter.closest('.contents'));
+
+// console.log(chapter.closest(''));
+
+// const input = document.getElementById('input')
+// const p = document.getElementById('p')
+// const removebnt = document.getElementById('remove')
+
+// removebnt .setAttribute('disabled', true)
+
+
+// input.style.color = 'red'
+// input.style.backgroundcolor = 'green'
+
+
+// const inputfunk = () => { 
+//     console.log(input.value);
+//      p.innerHTML = input.value
+//      if (input.value.length < 1) {
+//         removebnt.setAttribute('', true)
+
+//      } else removebnt.removeAttribute('disabled')
+    
+//     }
+
+// input.addEventListener('input', inputfunk)
+
+// removebnt.onclick = () => { alert('remove listener')
+// alert(`last value is: , ${input.value}`)
+// input.removeEventListener('input', inputfunk)
+// p.innerHTML = ' '
+// }
+
+// let zadanie = document.querySelector('.input')
+
+// let arr = ('' + zadanie)
+
+// console.log(arr);
+
+
+// const raschet = function R(q, w, e, r, t, y, u, i, o) {
+//     let Rb = ((-q*w-(e*r)*t+y)/u)+o
+//     let Ra = (-y-(e*r)*t+q*i)/u
+//     console.log('rb=', Rb,'kH');
+//     console.log('ra=', Ra,'kH');
+
+//     let proverka = -q+Ra+(e*r)+Rb
+//     console.log('proverka=', proverka,'~ 0 => verno');
+// }
+
+
+
+
+
